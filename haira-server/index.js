0014998078f1b9c8dcb179f34259462c4c928448
Landy Rakotoarison <landytiaa@gmail.com>
2025-10-15 21:31:45 -0400

    React and express setup

---
+++ haira-server/index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import cors from 'cors';
+
+
+import homeRoutes from './routes/HomeRoutes.js';
+
+const app = express();
+const port = 3002;
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Logging middleware to debug requests
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path}`);
+  next();
+});
+
+// add all routes here 
+app.use('/api', homeRoutes);
+
+
+
+app.listen(port, () => {
+  console.log(`🔌 Server is running on http://localhost:${port}`);
+});
